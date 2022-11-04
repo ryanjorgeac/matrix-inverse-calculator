8fda409fde983030263349445ac9a5c0970d344e
Ryan Carvalho <ryan.abreu@dcx.ufpb.br>
2022-11-04 20:24:54 -0300

    The main is working good.

--- matrixCalculator.py
+++ matrixCalculator.py
@@ -1,11 +1,18 @@
 import inputIO
-from matrixAsker import *
+from escalation import *
 
 def main(userIO):
     size = int(userIO.input("Informe o tamanho da Matriz: "))
     matriz = askMatrix(size, userIO)
     viewMatrix(size, matriz, userIO)
-    print("Acabou aqui.")
+    print("\nComeçando o escalonamento...\n")
+    result = escalation(matriz)
+    print("Matriz: ")
+    viewMatrix(len(result[0]), result[0], userIO)
+    print("")
+    print("Inversa: ")
+    viewMatrix(len(result[1]), result[1], userIO)
+
 
 if __name__ == "__main__":
     main(inputIO.inputIO())
