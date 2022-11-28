b44e57263079c1814439313ebded5baafce4fabf
Ryan Carvalho <ryan.abreu@dcx.ufpb.br>
2022-11-28 20:07:41 -0300

    Class matrix implemented.

---
+++ matrix.py
@@ -0,0 +1,18 @@
+class Matrix:
+    def __init__(self, matrix: list):
+        self.matrix = matrix
+
+    def __getitem__(self, line):
+        return self.matrix[line]
+
+    def __setitem__(self, line, value):
+        self.matrix[line] = value
+
+    def __eq__(self, otherMatrix):
+        return self.matrix == otherMatrix.matrix
+
+    def numberOfLines(self):
+        return len(self.matrix)
+
+    def numberOfColumns(self):
+        return len(self.matrix[0])
