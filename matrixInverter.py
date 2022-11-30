0d61c622fb8250c1c1dc7488cd1e8d56febf1ef8
Ryan Carvalho <ryan.abreu@dcx.ufpb.br>
2022-11-29 21:39:07 -0300

    A inverter of matrix has been created to separate escalation from inversion of a matrix, even if they almost do the same job.

---
+++ matrixInverter.py
@@ -0,0 +1,18 @@
+from copy import deepcopy
+
+import augmentedMatrix
+import escalation
+from matrixAsker import makeIdentityMatrix
+from maybe import just, nothing
+
+
+def inverter(matrix):
+    copyOfMatrix = deepcopy(matrix)
+    identityMatrixToCalculate = makeIdentityMatrix(matrix.numberOfLines())
+    augMatrix = augmentedMatrix.AugmentedMatrix(copyOfMatrix, identityMatrixToCalculate)
+    escalatedMatrix = escalation.escalationOfAugmentedMatrix(augMatrix)
+    if escalatedMatrix.firstMatrix() == makeIdentityMatrix(matrix.numberOfLines()):
+        return just(escalatedMatrix.secondMatrix())
+
+    else:
+        return nothing()
