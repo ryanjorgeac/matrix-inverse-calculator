962477064f7d044fc99f752a64cac3b2be1716f1
Ryan Carvalho <ryan.abreu@dcx.ufpb.br>
2022-11-29 23:23:45 -0300

    Main is being adapted for using the new classes and functions.

--- matrixCalculator.py
+++ matrixCalculator.py
@@ -3,6 +3,13 @@ from escalation import *
 
 
 def main(userIO):
+    menu()
+    opcao = input("         ")
+    if opcao == "3":
+        print('''
+            Até logo :)''')
+        return
+
     howManyLines = int(userIO.input("Informe a quantidade de linhas da Matriz: "))
     howManyColumns = int(userIO.input("Informe a quantidade de colunas da Matriz: "))
     matriz = askMatrix(howManyLines, howManyColumns, userIO)
@@ -16,6 +23,16 @@ def main(userIO):
         viewMatrix(result[1], userIO)
     except nnInvertibleException as error:
         print(error.args[0])
+
+
+def menu():
+    print('''
+    Bem-vindo(a) a Calculadora de Matrizes!
+            O que deseja calcular?
+            
+        [1] - Inverter uma Matriz
+        [2] - Escalonar uma Matriz
+        [3] - Sair''')
 
 
 if __name__ == "__main__":
